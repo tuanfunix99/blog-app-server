dc5b291ab9b6583007f0b45efb45988763c650dd
minggu99 <tuanphamtran99@gmail.com>
2022-01-12 22:47:40 +0700

    update feature delete post

--- src/Post/post.resolver.ts
+++ src/Post/post.resolver.ts
@@ -3,7 +3,7 @@ import Post from "./schema/Post";
 import cloudinary from "cloudinary";
 import { v4 as uuidv4 } from "uuid";
 import log from "../logger";
-import GraphQLJSON from 'graphql-type-json';
+import GraphQLJSON from "graphql-type-json";
 
 const JSon = GraphQLJSON;
 
@@ -11,10 +11,11 @@ const Query = {
   async post(parent: any, args: any) {
     const { input } = args;
     try {
-      const post = await Post.findOne({_id: input});
+      const post = await Post.findOne({ _id: input });
       post.content = JSON.stringify(post.content);
       return post;
     } catch (error) {
+      log.error(error.message, "Error get post");
       throw new Error("Not found");
     }
   },
@@ -24,11 +25,33 @@ const Query = {
         {},
         "title backgroundPic createdAt categories"
       )
-      .sort({ createdAt: -1 })
-      .populate("categories", "name")
-      .populate("createdBy", "username profilePic");
+        .sort({ createdAt: -1 })
+        .populate("categories", "name")
+        .populate("createdBy", "username profilePic");
       return posts;
     } catch (error) {
+      log.error(error.message, "Error get posts");
+      throw new Error("Not found");
+    }
+  },
+  async myPost(parent: any, args: any, context: any) {
+    const { input } = args;
+    const { req, res } = context;
+    try {
+      await auth(req, res);
+      if (res.locals.user._id != input) {
+        throw new Error("User not allow");
+      }
+      let posts = await Post.find(
+        { createdBy: input },
+        "title backgroundPic createdAt categories"
+      )
+        .sort({ createdAt: -1 })
+        .populate("categories", "name")
+        .populate("createdBy", "username profilePic");
+      return posts;
+    } catch (error) {
+      log.error(error.message, "Error get my post");
       throw new Error("Not found");
     }
   },
@@ -41,14 +64,14 @@ const Mutation = {
     let objectContent = <any>JSon.parseValue(input.content);
     try {
       await auth(req, res);
-      if(res.locals.user._id != input.userId) {
+      if (res.locals.user._id != input.userId) {
         throw new Error("User not allowed");
       }
       const post = new Post({
         title: input.title,
         content: objectContent,
         categories: input.categories,
-        createdBy: input.userId
+        createdBy: input.userId,
       });
       if (input.backgroundPic !== "./background.jpg") {
         const result = await (<any>uploadToCloudinary(input.backgroundPic));
@@ -58,6 +81,26 @@ const Mutation = {
       return post._id;
     } catch (error) {
       log.error(error.message, "Error publish post");
+      throw new Error("");
+    }
+  },
+
+  async deletePost(parent: any, args: any, context: any) {
+    const { req, res } = context;
+    const { input } = args;
+    try {
+      await auth(req, res);
+      const post = await Post.findOne({ _id: input }, "createdBy");
+      if(!post) {
+        throw new Error("Post not found");
+      }
+      if(post.createdBy.toString() !== res.locals.user._id.toString()) {
+        throw new Error("User not allowed");
+      }
+      await Post.findOneAndRemove({ _id: input });
+      return true;
+    } catch (error) {
+      log.error(error.message, "Error delete post");
       throw new Error("");
     }
   },
